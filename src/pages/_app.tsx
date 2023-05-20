d23f6ece183e593744b692ad8ff953b99ae7a6d5
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-20 11:34:14 +0530

    Chakra ui and clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { useRouter } from "next/router";
+
+const publicPages = ["/"];
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter();
+
+  const isPublicPage = publicPages.includes(pathname);
+  return (
+    <>
+      <Head>
+        <title>Nyalaan</title>
+      </Head>
+      <ClerkProvider {...pageProps}>
+        {isPublicPage ? (
+          <ChakraProvider>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        ) : (
+          <>
+            <SignedIn>
+              <ChakraProvider>
+                <Component {...pageProps} />
+              </ChakraProvider>
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn afterSignInUrl="/transcription" redirectUrl="/" />
+            </SignedOut>
+          </>
+        )}
+      </ClerkProvider>
+    </>
+  );
 }
---
+++ src/pages/components/Header.tsx
@@ -0,0 +1,40 @@
+import { Flex, Text, Button, Stack, Link } from "@chakra-ui/react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+export default function Header() {
+  return (
+    <>
+      <Flex
+        bg="white"
+        w="100%"
+        p={2}
+        verticalAlign="center"
+        as="header"
+        position="fixed"
+      >
+        <Text fontSize="3xl">Nyalaan</Text>
+
+        <Stack direction="row" ml="auto" alignItems="center">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Link href="/sign-in">
+              <Button
+                colorScheme={"green"}
+                bg={"green.400"}
+                rounded={"full"}
+                px={6}
+                _hover={{
+                  bg: "green.500",
+                }}
+              >
+                Sign In
+              </Button>
+            </Link>
+          </SignedOut>
+        </Stack>
+      </Flex>
+    </>
+  );
+}
